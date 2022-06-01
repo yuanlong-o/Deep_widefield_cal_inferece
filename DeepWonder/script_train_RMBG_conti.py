e64860a5ffbbb57bbd228f3d61f4290d4b84a770
GuoxunZhang <58133680+Galaxy-31416926535@users.noreply.github.com>
2022-06-01 11:14:32 +0800

    Add files via upload

---
+++ DeepWonder/script_train_RMBG_conti.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# train RMBG model
+os.system('python train_RMBG_model_conti.py --datasets_folder train_RMBG1 \
+                               --n_epochs 2 --GPU 0 --batch_size 1 \
+                               --fmap 32 --lr 0.0001 \
+                               --denoise_model arr_202110011541 --pth_name E_30_Iter_4009 \
+                               --img_h 128 --img_w 128 --img_s 128 \
+                               --train_datasets_size 4000')
